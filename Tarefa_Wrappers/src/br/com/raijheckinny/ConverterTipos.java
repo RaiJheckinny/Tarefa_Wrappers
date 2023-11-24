30ee866b50b3bc69b15d6cf387f9e20a3fd4a687
Rai Jheckinny <121577561+RaiJheckinny@users.noreply.github.com>
2023-11-24 07:37:07 -0300

    Estudado convesao de tipos

---
+++ Tarefa_Wrappers/src/br/com/raijheckinny/ConverterTipos.java
@@ -0,0 +1,12 @@
+package br.com.raijheckinny;
+
+public class ConverterTipos {
+    public static void main(String args[]){
+        //Converter variavel primitiva para variavel Wrappers
+        int indentidade = 292837777;
+        Integer identidadeWrappers = indentidade;
+
+        System.out.println(identidadeWrappers);
+
+    }
+}
